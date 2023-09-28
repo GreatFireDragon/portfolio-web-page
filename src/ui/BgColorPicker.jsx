8c6750edf2485691f516c5d1b1d24d9551dfedb9
greatfiredragon <kulmike2002@gmail.com>
2023-09-28 15:02:22 +0300

    new scrollbar

--- src/ui/BgColorPicker.jsx
+++ src/ui/BgColorPicker.jsx
@@ -32,8 +32,9 @@ function BgColorPicker() {
   useEffect(
     function () {
       $("body").css("background-color", bgColor);
+      // document.documentElement.style.setProperty("--scrollbar-color", bgColor);
     },
-    [bgColor]
+    [bgColor, style]
   );
 
   function handleChangeColor(color) {
@@ -46,36 +47,41 @@ function BgColorPicker() {
   }
 
   return (
-    <div className="absolute text-right top-5 right-3">
-      <div
-        onClick={handleButtonClick}
-        className="inline-block p-1 md:p-1.5 hover:scale-105 shadow-md cursor-pointer w-min bg-slate-50"
-      >
+    <>
+      <div className="absolute text-right top-5 right-3">
         <div
-          className="w-8 h-5 rounded-sm sm:w-12 sm:h-7 md:w-16 md:h-8"
-          style={{ backgroundColor: bgColor }}
-        ></div>
-      </div>
-
-      {colorPickerTutorial ? (
-        <div color={bgColor} className="flex items-center p-2 my-2 rounded-md bg-cyan-50">
-          Change theme color!&nbsp;
-          <Icon icon="akar-icons:pointer-hand" />
+          onClick={handleButtonClick}
+          className="inline-block p-1 md:p-1.5 hover:scale-105 shadow-md cursor-pointer w-min bg-slate-50"
+        >
+          <div
+            className="w-8 h-5 rounded-sm sm:w-12 sm:h-7 md:w-16 md:h-8"
+            style={{ backgroundColor: bgColor }}
+          ></div>
         </div>
-      ) : null}
 
-      {displayColorPicker ? (
-        <div className="my-5 z-2">
-          <TwitterPicker
-            triangle="top-right"
-            width="200px"
+        {colorPickerTutorial ? (
+          <div
             color={bgColor}
-            colors={bgColorsList}
-            onChangeComplete={handleChangeColor}
-          />
-        </div>
-      ) : null}
-    </div>
+            className="flex items-center p-2 my-2 rounded-md bg-cyan-50"
+          >
+            Change theme color!&nbsp;
+            <Icon icon="akar-icons:pointer-hand" />
+          </div>
+        ) : null}
+
+        {displayColorPicker ? (
+          <div className="my-5 z-2">
+            <TwitterPicker
+              triangle="top-right"
+              width="200px"
+              color={bgColor}
+              colors={bgColorsList}
+              onChangeComplete={handleChangeColor}
+            />
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 }
 
